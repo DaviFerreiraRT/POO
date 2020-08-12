939a8cb65a077352a3eaaf044e418624a2834d6c
Davi Ferreira <daviferreirart@gmail.com>
2020-08-12 13:42:04 -0300

    Initial commit

---
+++ POO/src/Classes.java
@@ -0,0 +1,16 @@
+
+public class Classes {
+	int dia;
+	int mes;
+	int ano;
+
+	public static void main(String[] args) {
+
+		Classes hoje = new Classes();
+		hoje.dia = 04;
+		hoje.mes = 8;
+		hoje.ano = 2020;
+		System.out.println(hoje.dia + "/" + hoje.mes + "/" + hoje.ano);
+
+	}
+};
