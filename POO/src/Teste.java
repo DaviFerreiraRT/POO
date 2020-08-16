52cfb45a36d127ba060e6d515611368e215dc113
Davi Ferreira <daviferreirart@gmail.com>
2020-08-16 10:41:54 -0300

    update

--- POO/src/Teste.java
+++ POO/src/Teste.java
@@ -4,9 +4,15 @@ public class Teste {
         hoje.dia=13;
         hoje.mes=8;
         hoje.ano=2020;
-        hoje.exibir();
+        System.out.println("Dia do ano: "+hoje.dia);
+        System.out.println("O mes do ano: "+hoje.mes);
+        System.out.println("O ano: "+hoje.ano);
+    
+        //Nova instancia de hoje é criada
         hoje=new Data();
         hoje.dia=21;
+        hoje.mes=5;
+        hoje.ano=2019;
         hoje.exibir();
     }
 }
