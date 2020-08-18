181fd0d3a2fe8eb2d83e152c4b1a84906236b4b8
Davi Ferreira <daviferreirart@gmail.com>
2020-08-18 09:38:03 -0300

    Update

--- POO/src/Data.java
+++ POO/src/Data.java
@@ -1,9 +1,46 @@
 public class Data {
+    public Data (int x){
+        x=ano;
+    }
+    public Data(int m, int n, int p) {
+        dia = m;
+        mes = n;
+        ano = p;
+    }
+
+    public Data() {
+        dia = 14;
+        mes = 5;
+        ano = 2015;
+    }
+
+    // Variaveis de instancia;
     int dia;
     int mes;
     int ano;
 
-    public void exibir() {
+    // Metodos
+    public void exibir(String Mensagem) {
+        System.out.println(Mensagem);
+        exibir();
+
+    }
+
+    void exibir() {
         System.out.println(dia + "/" + mes + "/" + ano);
+    }
+
+    int diasDoano() {
+        int dias = 0;
+        return dias;
+    }
+
+    void incrementarAno(int nAnos) {
+
+        ano += nAnos;
+    }
+
+    void incrementarAno() {
+        ano++;
     }
 };
--- POO/src/Teste.java
+++ POO/src/Teste.java
@@ -1,18 +1,33 @@
 public class Teste {
     public static void main(String[] args) {
-        Data hoje=new Data();
-        hoje.dia=13;
-        hoje.mes=8;
-        hoje.ano=2020;
-        System.out.println("Dia do ano: "+hoje.dia);
-        System.out.println("O mes do ano: "+hoje.mes);
-        System.out.println("O ano: "+hoje.ano);
-    
-        //Nova instancia de hoje é criada
-        hoje=new Data();
-        hoje.dia=21;
-        hoje.mes=5;
-        hoje.ano=2019;
-        hoje.exibir();
+        Data hoje = new Data();
+        /*
+         * hoje.dia = 13; hoje.mes = 8; hoje.ano = 2020;
+         */
+        hoje.exibir("Criado");
+        /*
+         * System.out.println("Dia do ano: " + hoje.dia);
+         * System.out.println("O mes do ano: " + hoje.mes); System.out.println("O ano: "
+         * + hoje.ano);
+         */
+
+        // Nova instancia de hoje é criada
+        hoje = new Data();
+        hoje.dia = 21;
+        hoje.mes = 5;
+        hoje.ano = 2019;
+        hoje.exibir("Antes de incrementar, data original");
+        hoje.incrementarAno();
+        hoje.exibir("Depois de Incrementar");
+        hoje.incrementarAno(3);
+        hoje.exibir("Depois de incrementar 3 anos");
+
+        Data nova = new Data (18,8,2020);
+
+        nova.exibir("Nova data");
+
+        Data outroAno=new Data(2010);
+        outroAno.exibir("Criando somente ano ");
+
     }
 }
