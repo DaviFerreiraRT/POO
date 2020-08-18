9ee8ff89b1fe34c02add7878cbcbc67813bbdd96
Davi Ferreira <daviferreirart@gmail.com>
2020-08-18 12:21:25 -0300

    Code

--- Atividade Estudante/src/Estudante.java
+++ Atividade Estudante/src/Estudante.java
@@ -8,9 +8,13 @@ public class Estudante {
         System.out.println("Matricula do aluno(a)= " + this.matricula);
         System.out.println("Nome do aluno(a)= " + this.nome);
         System.out.println("Sexo do aluno(a)= " + this.sexo);
+        double soma=0;      
         for (int i = 0; i < notas.length; i++) {
-            System.out.println("Notas do aluno(a)=" + this.notas[i]);
+            soma+=notas[i];
+            System.out.println("Todas as notas do aluno(a)=" + this.notas[i]);
         }
+        double media=soma/notas.length;
+        System.out.println("A média do aluno é= "+media);
     }
 
     void atribuirNota(int numProva, double nota) {
--- Atividade Estudante/src/testeEstudante.java
+++ Atividade Estudante/src/testeEstudante.java
@@ -5,16 +5,14 @@ public class testeEstudante {
         e.matricula = 2004001;
         e.nome = "Maria Silva";
         e.sexo = 'F';
-        /*e.notas[0]=7.0;
-        e.notas[1]=8.2;
-        e.notas[2]=6.0;
-        e.notas[3]=5.5;*/
-        e.atribuirNota(1,7.0);
-        e.atribuirNota(2,8.2);
-        e.atribuirNota(3,6.0);
-        e.atribuirNota(4,5.5);
-        double nota1=e.lerNota(2);
-        System.out.println("Nota 1 = "+nota1);
+
+        e.atribuirNota(1, 7.0);
+        e.atribuirNota(2, 8.2);
+        e.atribuirNota(3, 6.0);
+        e.atribuirNota(4, 5.5);
+
+        System.out.println("Nota requisitada= "+e.lerNota(1));
         e.exibir();
+
     }
 }
