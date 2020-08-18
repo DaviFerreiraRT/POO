1d04fc12cbac870a1ec60138519e27d86eaf41df
Davi Ferreira <daviferreirart@gmail.com>
2020-08-18 13:25:39 -0300

    Update

--- Atividade Estudante/src/Estudante.java
+++ Atividade Estudante/src/Estudante.java
@@ -1,4 +1,18 @@
 public class Estudante {
+    public Estudante(String nom, char sex, int matri) {
+        this.matricula = matri;
+        this.nome = nom;
+        this.sexo = sex;
+    }
+
+    public Estudante(String nome, char sexo) {
+        this.nome = nome;
+        this.sexo = sexo;
+    }
+
+    public Estudante() {
+    }
+
     int matricula;
     String nome;
     char sexo;
@@ -8,17 +22,36 @@ public class Estudante {
         System.out.println("Matricula do aluno(a)= " + this.matricula);
         System.out.println("Nome do aluno(a)= " + this.nome);
         System.out.println("Sexo do aluno(a)= " + this.sexo);
-        double soma=0;      
+        double soma = 0;
         for (int i = 0; i < notas.length; i++) {
-            soma+=notas[i];
+            soma += notas[i];
             System.out.println("Todas as notas do aluno(a)=" + this.notas[i]);
         }
-        double media=soma/notas.length;
-        System.out.println("A média do aluno é= "+media);
+        double media = soma / notas.length;
+        System.out.println("A média do aluno é= " + media);
+    }
+
+    void exibir(String titulo) {
+        System.out.println(titulo);
+        System.out.println("Matricula do aluno(a)= " + this.matricula);
+        System.out.println("Nome do aluno(a)= " + this.nome);
+        System.out.println("Sexo do aluno(a)= " + this.sexo);
+        double soma = 0;
+        for (int i = 0; i < notas.length; i++) {
+            soma += notas[i];
+            System.out.println("Todas as notas do aluno(a)=" + this.notas[i]);
+        }
+        double media = soma / notas.length;
+        System.out.println("A média do aluno é= " + media);
     }
 
     void atribuirNota(int numProva, double nota) {
         this.notas[numProva - 1] = nota;
+
+    }
+
+    void atribuirNota(int indice) {
+        this.notas[indice] = 0.0;
     }
 
     double lerNota(int numProva) {
