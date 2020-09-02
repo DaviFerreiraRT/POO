b2028bae5d1fc53bec3861f0b95b8089efe19ddc
Davi Ferreira <daviferreirart@gmail.com>
2020-09-02 15:56:18 -0300

    commit

--- Revisão/Remedio.java
+++ Revisão/Remedio.java
@@ -6,45 +6,48 @@ public class Remedio {
     private int validade;
     private double preco;
 
-    public Remedio(){
-        
+    public Remedio() {
+
     }
 
-    public Remedio(int codigo, String nome){
-        this.codigo=codigo;
-        this.nome=nome;
-        
-    }
-    public Remedio(int codigo, String nome,String dataDeFabricacao){
-        this.codigo=codigo;
-        this.nome=nome;
-        this.dataDeFabricacao=dataDeFabricacao;
-    }
-    public Remedio(int codigo, String nome,String dataDeFabricacao, int validade){
-        this.codigo=codigo;
-        this.nome=nome;
-        this.validade=validade;
-        this.dataDeFabricacao=dataDeFabricacao;
+    public Remedio(int codigo, String nome) {
+        this.codigo = codigo;
+        this.nome = nome;
+
     }
 
-    public Remedio(int codigo, String nome,String dataDeFabricacao, int validade, double preco){
-        this.codigo=codigo;
-        this.nome=nome;
-        this.validade=validade;
-        this.dataDeFabricacao=dataDeFabricacao;
-        this.preco=preco;
+    public Remedio(int codigo, String nome, String dataDeFabricacao) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.dataDeFabricacao = dataDeFabricacao;
+    }
+
+    public Remedio(int codigo, String nome, String dataDeFabricacao, int validade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.validade = validade;
+        this.dataDeFabricacao = dataDeFabricacao;
+    }
+
+    public Remedio(int codigo, String nome, String dataDeFabricacao, int validade, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.validade = validade;
+        this.dataDeFabricacao = dataDeFabricacao;
+        this.preco = preco;
     }
 
     public void reduzirPreco(double valor) {
         preco = preco - valor;
-        System.out.println("O novo valor do remédio é :" + preco);
+
     }
 
     public boolean ehValido(int data) {
+
         if (this.validade <= data) {
             return true;
-        } else {
-            
+        } 
+        else {
             return false;
         }
     }
@@ -52,7 +55,7 @@ public class Remedio {
     public void imprimir() {
         System.out.println("O codigo do produto é :" + codigo);
         System.out.println("O nome do produto é :" + nome);
-        System.out.println("A data do produto é "+dataDeFabricacao);
+        System.out.println("A data do produto é " + dataDeFabricacao);
         System.out.println("O preço do produto é de R$ " + preco);
         System.out.println("A validade do produto é de " + validade + " dias");
 
@@ -65,34 +68,43 @@ public class Remedio {
     public void aumentarPreco(double valor) {
         preco = preco + valor;
     }
-    
+
     public double getPreco() {
         return preco;
     }
+
     public String getNome() {
         return nome;
     }
+
     public String getDataDeFabricacao() {
         return dataDeFabricacao;
     }
+
     public int getCodigo() {
         return codigo;
     }
+
     public int getValidade() {
         return validade;
     }
+
     public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
+
     public void setDataDeFabricacao(String dataDeFabricacao) {
         this.dataDeFabricacao = dataDeFabricacao;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public void setPreco(double preco) {
         this.preco = preco;
     }
+
     public void setValidade(int validade) {
         this.validade = validade;
     }
