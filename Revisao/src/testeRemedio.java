25b64a8c3ece0868a96a1082670edb9a3a301764
Davi Ferreira <daviferreirart@gmail.com>
2020-09-02 12:39:04 -0300

    1st commit

--- Revisao/src/testeRemedio.java
+++
@@ -1,6 +0,0 @@
-public class testeRemedio {
-    public static void main(String[] args) {
-        Remedio teste = new Remedio();
-        System.out.println("teste");
-    }     
-}
---
+++ Revisão/testeRemedio.java
@@ -0,0 +1,13 @@
+public class testeRemedio {
+      public static void main(String[] args) {
+        
+        Remedio teste1=new Remedio(1234,"Dipirona");
+        teste1.setDataDeFabricacao("14/5/2020");
+        teste1.setPreco(55.60);
+        teste1.reduzirPreco(10.60);
+        teste1.setValidade(120);
+        teste1.imprimir();
+        
+      }
+    
+}
