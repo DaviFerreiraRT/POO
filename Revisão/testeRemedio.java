af9f37b97200eb7ebb69adf747c0f5408e64f6ed
Davi Ferreira <daviferreirart@gmail.com>
2020-09-02 12:50:16 -0300

    Update

--- Revisão/testeRemedio.java
+++ Revisão/testeRemedio.java
@@ -5,7 +5,7 @@ public class testeRemedio {
         teste1.setDataDeFabricacao("14/5/2020");
         teste1.setPreco(55.60);
         teste1.reduzirPreco(10.60);
-        teste1.setValidade(120);
+        teste1.setValidade(5);
         teste1.imprimir();
         
       }
