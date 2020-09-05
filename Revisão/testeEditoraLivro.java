60f7647e4ccc408b6b8f37b3329cad74f17b12ec
Davi Ferreira <daviferreirart@gmail.com>
2020-09-05 14:00:41 -0300

    update

--- Revisão/testeEditoraLivro.java
+++ Revisão/testeEditoraLivro.java
@@ -3,8 +3,7 @@ public class testeEditoraLivro {
         Livro livro= new Livro("Harry Potter, A Ordem da Fênix",6969,50.0);
         Editora editora = new Editora("JK ROLLING","London, UK",245748);
         livro.setEditora(editora);
-        livro.exibir();
         livro.getEditora().setEndereco("Manchester,UK ");
-        livro.exibir();
+        System.out.println(livro.getEditora().getCnpj());
     }
 }
