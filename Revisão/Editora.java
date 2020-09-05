f66ad041512394f63c73b5fcf8c3bf931afac9ed
Davi Ferreira <daviferreirart@gmail.com>
2020-09-05 12:09:02 -0300

    update

---
+++ Revisão/Editora.java
@@ -0,0 +1,40 @@
+public class Editora {
+    private String nome;
+    private String endereco;
+    private int cnpj;
+
+    public Editora ( String nome, String endereco, int cnpj){
+        this.nome=nome;
+        this.endereco=endereco;
+        this.cnpj=cnpj;
+
+    }
+    public Editora ( String nome, String endereco){
+        this.nome=nome;
+        this.endereco=endereco;
+    }
+
+    public void exibirEditora(){
+        System.out.println("Nome da editora: "+nome);
+        System.out.println("Endereco da editora: "+endereco);
+        System.out.println("CNPJ da editora: "+cnpj);
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setCnpj(int cnpj) {
+        this.cnpj = cnpj;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public int getCnpj() {
+        return cnpj;
+    }
+}
---
+++ Revisão/testeEditoraLivro.java
@@ -0,0 +1,8 @@
+public class testeEditoraLivro {
+    public static void main(String[] args) {
+        Livro livro= new Livro("Harry Potter, A Ordem da Fênix",6969,50.0);
+        Editora editora = new Editora("JK ROLLING","LONDRES",245748);
+        livro.setEditora(editora);
+        livro.exibir();
+    }
+}
