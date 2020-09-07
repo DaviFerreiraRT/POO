3a3830e803c58e0074b30923ed2c60d2fdb402d4
Davi Ferreira <daviferreirart@gmail.com>
2020-09-07 19:29:58 -0300

    commit

--- Prova/src/testeProva.java
+++ Prova/src/testeProva.java
@@ -1,12 +1,12 @@
 public class testeProva {
-private double media;
+
 private double n1,n2,n3;
-
-
-
-public double getMedia() {
-	return media;
+public testeProva(double n1, double n2, double n3){
+	this.n1=n1;
+	this.n2=n2;
+	this.n3=n3;
 }
+
 public double getN1() {
 	return n1;
 }
@@ -17,9 +17,7 @@ public double getN2() {
 public double getN3() {
 	return n3;
 }
-public void setMedia(double media) {
-	this.media = media;
-}
+
 public void setN1(double n1) {
 	this.n1 = n1;
 }
@@ -29,8 +27,16 @@ public void setN2(double n2) {
 public void setN3(double n3) {
 	this.n3 = n3;
 }
+double resultado;
+public void calculaMedia(){
 
-public double calculaMedia(double n1,double n2, double n3){
-	return this.media=(n1+n2*2+n3*3)/6;
+	resultado=(n1+n2+n3)/3;;
+	
+}
+public void exibir(){
+	System.out.println("Nota 1: "+n1);
+	System.out.println("Nota 2: "+n2);
+	System.out.println("Nota 3: "+n3);
+	System.out.println("MEDIA FINAL FOI : "+resultado);
 }
 }
