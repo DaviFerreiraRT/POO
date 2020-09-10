1c524149e062002d28b4bf78d3a18801172d8381
Davi Ferreira <daviferreirart@gmail.com>
2020-09-10 12:56:13 -0300

    update

--- Atividade Estudante/src/Turma.java
+++ Atividade Estudante/src/Turma.java
@@ -1,28 +1,86 @@
 public class Turma {
-    
-Estudante estudantes[] = new Estudante[10];
-String nome;
-private int numEstudantes = 0;
 
-public Turma (){}
-public Turma ( String nome ) { 
-    this.nome = nome; 
-}
+    private Estudante estudantes[] = new Estudante[10];
+    private String nome;
+    private int numEstudantes = 0;
 
-public void matricular (Estudante e) {
-estudantes[numEstudantes] = e;
-numEstudantes++;
-}
+    public Turma() {
+    }
 
-public int getNumEstudantes() {
-    return numEstudantes;
-}
-public void exibir(){
-    for(int i=0;i<estudantes.length;i++){
-        if(estudantes[i]!=null){
-        System.out.println("Dados do aluno: "+estudantes[i]);
+    public Turma(String nome) {
+        this.nome = nome;
+    }
+
+    public void matricular(Estudante e) {
+        estudantes[numEstudantes] = e;
+        numEstudantes++;
+    }
+
+    public int getNumEstudantes() {
+        return numEstudantes;
+    }
+
+    public void setEstudantes(Estudante[] estudantes) {
+        this.estudantes = estudantes;
+    }
+
+    public void setNumEstudantes(int numEstudantes) {
+        this.numEstudantes = numEstudantes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Estudante[] getEstudantes() {
+        return estudantes;
+    }
+
+    public void exibir() {
+        for (int i = 0; i < estudantes.length; i++) {
+            if (estudantes[i] != null) {
+                System.out.println("-----------" + nome + "-----------");
+                estudantes[i].Exibir();
+
+            }
+        }
+    }
+
+    public Estudante pesquisar(int matricula) {
+        for (int i = 0; i < numEstudantes; i++) {
+            if (estudantes[i] != null && estudantes[i].getMatricula() == matricula) {
+                System.out.println("Numero da matricula encontrada! \nNr da matricula requsitada: " + matricula);
+                return estudantes[i];
+            }
+        }
+        System.out.println("Matricula não encontrada!");
+        return null;
+    }
+
+    public void trancar(int matricula) {
+        boolean matriculaExiste = false;
+        int index = 0;
+
+        for (int i = 0; i < numEstudantes; i++) {
+            if (estudantes[i].getMatricula() == matricula) {
+                matriculaExiste = true;
+                index = i;
+                break;
+
+            }
+
+        }
+        if (matriculaExiste) {
+            for (int i = index; i < estudantes.length - 1; i++) {
+                estudantes[i] = estudantes[i + 1];
+                
+            }
+            estudantes[estudantes.length - 1] = null;
+            numEstudantes--;
+        }
+        else{
+            System.out.println("Matricula não existe! ");
+        }
+        
     }
 }
-}
-}
-
--- Atividade Estudante/src/testeTurma.java
+++ Atividade Estudante/src/testeTurma.java
@@ -1,10 +1,30 @@
 public class testeTurma {
  public static void main(String[] args) {
-     Estudante novoEstudante = new Estudante("Maria",'F');
-     Turma poo = new Turma("Programação Orientadas a Objetos");
-     poo.matricular(novoEstudante);
-     novoEstudante.Exibir();
-     novoEstudante = new Estudante("Davi",'M');
+     Estudante estudante = new Estudante("Maria",'F');
+     Turma poo = new Turma("Programação Orientada a Objetos");
+     estudante.setMatricula(5);
+     estudante.setNota(1,5);
+     estudante.setNota(2,7.5);
+     estudante.setNota(3,8.5);
+     estudante.setNota(4,10);
+     poo.matricular(estudante);
+     estudante=new Estudante ("Davi",'M');
+     estudante.setMatricula(1);
+     poo.matricular(estudante);
+     
+     /*Estudante segundoEstudante = new Estudante("Davi",'M');
+     poo.matricular(segundoEstudante);*/
+     //poo.exibir();
+
+     poo.pesquisar(5);
+     poo.pesquisar(1);
+     poo.trancar(5);
+     //poo.exibir();
+     poo.trancar(1);
      poo.exibir();
+    
+
+
+     
  }   
 }
