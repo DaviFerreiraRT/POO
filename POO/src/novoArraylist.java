4275dacc9a5927578cc455fee1c2f2754f13866a
Davi Ferreira <daviferreirart@gmail.com>
2020-09-10 19:48:32 -0300

    update

---
+++ POO/src/novoArraylist.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+public class novoArraylist {
+   public static void main(String[] args) {
+    ArrayList <Pessoa> testeArray = new ArrayList<>();       
+    System.out.println(testeArray.isEmpty());
+    JOptionPane.showMessageDialog(null, "analise");
+}
+
+}   
