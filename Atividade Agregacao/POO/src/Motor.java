aac5bc7a652c4dc9456ea83102672a12e3138ed4
Davi Ferreira <daviferreirart@gmail.com>
2020-09-23 16:23:22 -0300

    1st commit

---
+++ Atividade Agregacao/POO/src/Motor.java
@@ -0,0 +1,37 @@
+public class Motor {
+	private double potencia;
+	private int cilindrada;
+
+	public Motor() {
+
+	}
+
+	public Motor(double potencia, int cilindrada) {
+		this.potencia = potencia;
+		this.cilindrada = cilindrada;
+		setPotencia(potencia);
+		setCilindrada(cilindrada);
+	}
+
+	public double getPotencia() {
+		return potencia;
+	}
+
+	public void setPotencia(double potencia) {
+		this.potencia = potencia;
+	}
+
+	public int getCilindrada() {
+		return cilindrada;
+	}
+
+	public void setCilindrada(int cilindrada) {
+		this.cilindrada = cilindrada;
+	}
+
+	public void exibir() {
+		System.out.println("Potencia do veiculo: " + potencia);
+		System.out.println("Cilindradas do veiculo: " + cilindrada);
+
+	}
+}
