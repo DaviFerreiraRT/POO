bf53a99d46d4a2b54b7b2378a985a03c06b39c5d
Davi Ferreira <daviferreirart@gmail.com>
2020-09-24 08:29:55 -0300

    update

---
+++ Lists/testeArrayList.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class testeArrayList {
+
+        ArrayList al = new ArrayList();
+        al.add(10);
+        a1.add("POO");
+        a1.add("Maria")
+
+
+    }
+   
+    public static void print ( ArrayList a){
+        for(int i =0 ; i<size(al);i++){
+            sysout
+        }
+    }
+}
