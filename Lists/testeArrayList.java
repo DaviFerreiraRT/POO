8fb2749aa1f39677bca644903e2c3043b2204650
Davi Ferreira <daviferreirart@gmail.com>
2020-09-24 08:32:15 -0300

    commit

--- Lists/testeArrayList.java
+++ Lists/testeArrayList.java
@@ -2,17 +2,19 @@ import java.util.*;
 
 public class testeArrayList {
 
+     public static void main(String[] args) {
+         
         ArrayList al = new ArrayList();
         al.add(10);
-        a1.add("POO");
-        a1.add("Maria")
-
+        al.add("POO");
+        al.add("Maria")
+        print(al)
 
     }
-   
+}  
     public static void print ( ArrayList a){
         for(int i =0 ; i<size(al);i++){
-            sysout
+            System.out.print(a.get(i))
         }
     }
 }
