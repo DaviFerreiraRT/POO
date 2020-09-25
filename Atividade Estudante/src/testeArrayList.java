ac40ceec84fc8830751cfb8807b1ee2b1619d97d
Davi <daviferreirart@gmail.com>
2020-09-25 20:54:23 -0300

    update

--- Atividade Estudante/src/testeArrayList.java
+++ Atividade Estudante/src/testeArrayList.java
@@ -5,19 +5,27 @@ public class testeArrayList {
         
         Turma t1 = new Turma ("POO");
         ArrayList al = t1.getEstudantes();
-
+        Curso c1= new Curso ("Sistemas de Informação","GSI",3600);
         Estudante e1 = new Estudante("Jonas", 'M', 154);
         Estudante e2 = new Estudante("Rodrigo", 'M', 155);
         Estudante e3 = new Estudante("Davi", 'M', 156);
         Estudante e4 = new Estudante("Vinicius", 'M', 157);
         Estudante e5 = new Estudante("Emily", 'F', 158);
         Estudante e6 = new Estudante("Beatriz", 'F', 159);
-
+        
+        e1.setMeuCurso(c1);
+        e2.setMeuCurso(c1);
+        e3.setMeuCurso(c1);
+        e4.setMeuCurso(c1);
+        e5.setMeuCurso(c1);
+        e6.setMeuCurso(c1);
         e6.setNota(1, 10);
         e6.setNota(2, 9);
         e6.setNota(3, 9.5);
         e6.setNota(4,8.0);
         Turma t2 = new Turma ( "BD2 ");
+
+        
         
     
 
