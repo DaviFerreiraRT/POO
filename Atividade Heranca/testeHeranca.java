0c01ab35a9f6e0506f99bc29bb109f2f982e7ebf
Davi <daviferreirart@gmail.com>
2020-10-06 11:55:44 -0300

    UPDATE

--- Atividade Heranca/testeHeranca.java
+++ Atividade Heranca/testeHeranca.java
@@ -1,12 +1,14 @@
 public class testeHeranca {
     public static void main(String[] args) {
         EstudanteMonitor em = new EstudanteMonitor(111, "Davi", 'M', 500.00, "POO");
+
         em.atribuirNota(1,10);
         em.atribuirNota(2,8);
         em.atribuirNota(3,9);
-        em.atribuirNota(4,7.5);  
+        em.atribuirNota(4,7); 
         
-        em.Exibir();
+        em.Exibir("----DADOS DO ALUNO----");
+
         em.tirarDuvidas();
         em.auxiliaProfessor();
 
@@ -16,6 +18,8 @@ public class testeHeranca {
         ee.atribuirNota(2,8);
         ee.atribuirNota(3,9);
         ee.atribuirNota(4,7.5);
+
+        System.out.println("---------------------------------------");
         ee.Exibir("Dados do esutudante");
         ee.trabalhar();
     }
