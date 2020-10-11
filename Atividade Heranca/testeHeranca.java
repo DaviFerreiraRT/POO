7fdc75052c84cd74b7fe9331c8e4308a5f9caabc
Davi <daviferreirart@gmail.com>
2020-10-11 10:07:56 -0300

    teste

--- Atividade Heranca/testeHeranca.java
+++ Atividade Heranca/testeHeranca.java
@@ -1,29 +1,42 @@
 public class testeHeranca {
     public static void main(String[] args) {
-        Estudante e = new Estudante("Davi",'M',101);
-        e.Exibir("----DADOS DO ALUNO ----");
+        Estudante e = new Estudante("Vinicius",'M',101);
+        e.atribuirNota(1, 10);
+        e.atribuirNota(2, 7);
+        e.atribuirNota(3, 5);
+        e.atribuirNota(4, 10);
+        //e.Exibir("----DADOS DO ALUNO ----");
 
-        EstudanteMonitor em = new EstudanteMonitor(111, "Davi", 'm', 500.00, "POO");
+        EstudanteMonitor em = new EstudanteMonitor(111, "Rodrigo", 'M', 500.00, "POO");
 
         em.atribuirNota(1,10);
         em.atribuirNota(2,8);
         em.atribuirNota(3,9);
         em.atribuirNota(4,7); 
         
-        em.Exibir("----DADOS DO ALUNO----");
+        //em.Exibir("----DADOS DO ALUNO----");cannot be resolved to a type
 
         em.tirarDuvidas();
         em.auxiliaProfessor();
 
 
-        EstudanteEstagiario ee= new EstudanteEstagiario(112, "Davi", 'M', 500, "AMAZON");
+        EstudanteEstagiario ee= new EstudanteEstagiario(112, "Jonas", 'M', 500, "AMAZON");
         ee.atribuirNota(1,8);
         ee.atribuirNota(2,8);
         ee.atribuirNota(3,9);
         ee.atribuirNota(4,7.5);
 
-        System.out.println("---------------------------------------");
-        ee.Exibir("Dados do esutudante");
-        ee.trabalhar();
+        //System.out.println("---------------------------------------");
+        //ee.Exibir("Dados do esutudante");
+        //ee.trabalhar();
+        Turma t1 = new Turma ("POO");
+        t1.matricular(ee);
+        t1.matricular(em);
+        t1.matricular(e);
+        t1.exibir();
+    
+    
+
+        
     }
 }
