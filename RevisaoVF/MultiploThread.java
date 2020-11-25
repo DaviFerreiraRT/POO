4cf4810869fa5ae22011798876f2b5a11b768f64
Davi <daviferreirart@gmail.com>
2020-11-25 09:56:31 -0300

    commit

---
+++ RevisaoVF/MultiploThread.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class MultiploThread extends Thread {
+    private static int numero;
+    static Scanner sc = new Scanner(System.in);
+
+    @Override
+    public void run() {
+
+        try {
+            System.out.println("---Procurando divisores---");
+            Thread.sleep(1000);
+            for (int i = numero; i > 0; i--) {
+                if (numero % i == 0) {
+                    System.out.println(numero + " divido " + i + " igual a " + numero / i);
+                }
+            }
+
+        } catch (InterruptedException e) {
+            System.out.println("erro");
+
+        }
+    }
+
+    public static void main(String[] args) {
+        MultiploThread t = new MultiploThread();
+        System.out.print("Digite um numero para buscar os multiplos e divisores: ");
+        numero = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            if (numero % i == 0) {
+                System.out.println("Multiplo de " + numero + " vezes " + i + " é igual a: " + numero * i);
+            }
+        }
+        t.start();
+    }
+}
